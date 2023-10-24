dced0e69a4886a97335e50154f5b28b0b1ad0ea3
Sergey Zaytsev <surgery_333@mail.ru>
2023-10-24 17:16:42 +0300

    Realised generating of player healthbar/enemy healthbar and bar generating refactored

--- Assets/_Scripts/Managers/MenuManager.cs
+++ Assets/_Scripts/Managers/MenuManager.cs
@@ -6,12 +6,15 @@ using UnityEngine.UI;
 public class MenuManager : MonoBehaviour, IMenuManager {
     public static MenuManager Instance;
     private static int playerMaxHealth = 10;
+    private static int enemyMaxHealth = 10;
     private Health playerHealth = new Health(playerMaxHealth);
-    private Health enemyHealth = new Health(10);
-    private Image healthBarSprite;
+    private Health enemyHealth = new Health(enemyMaxHealth);
+    private Image playerHealthSprite;
+    private Image enemyHealthSprite;
 
     [SerializeField] private GameObject _selectedHeroObject,_tileObject,_tileUnitObject;
-    [SerializeField] private GameObject _healthBarPrefab;
+    [SerializeField] private GameObject _alliedHealthPrefab;
+    [SerializeField] private GameObject _enemyHealthPrefab;
     // Поле ниже подтягивает Canvas(именно Transform Канваса) из иерархии в Unity.
     // Оно нужно для того, чтобы разместить healthBar ввиде дочернего объекта в Canvas по иерархии на сцене.
     [SerializeField] private Transform _canvas;
@@ -19,18 +22,37 @@ public class MenuManager : MonoBehaviour, IMenuManager {
     public void DamagePlayer(int damage)
     {
         int currentHealth = playerHealth.GetDamage(damage);
-        UpdateHealthBar(playerMaxHealth, currentHealth);
+        UpdateHealthBar(playerMaxHealth, currentHealth, Faction.Hero);
     }
 
-    public void GenerateHealthBar()
-    {   
-        healthBarSprite = Object.Instantiate(_healthBarPrefab, _canvas.transform).transform
-            .GetChild(0).GetComponent<Image>();
-    }
-
-    private void UpdateHealthBar(float maxHealth, float currentHealth)
+    public void DamageEnemy(int damage)
     {
-        healthBarSprite.fillAmount = currentHealth / maxHealth;
+        int currentHealth = enemyHealth.GetDamage(damage);
+        UpdateHealthBar(enemyMaxHealth, currentHealth, Faction.Enemy);
+    }
+
+    public void GenerateHealthBars()
+    {
+        GameObject playerHealthBar = Object.Instantiate(_alliedHealthPrefab, _canvas.transform);
+        playerHealthBar.transform.localPosition = new Vector3(0, _canvas.GetComponent<RectTransform>().sizeDelta.y / -2, 0);
+        playerHealthSprite = playerHealthBar.transform.GetChild(0).GetComponent<Image>();
+
+        GameObject enemyHealthBar = Object.Instantiate(_enemyHealthPrefab, _canvas.transform);
+        enemyHealthBar.transform.localPosition = new Vector3(0, _canvas.GetComponent<RectTransform>().sizeDelta.y / 2, 0);
+        enemyHealthSprite = enemyHealthBar.transform.GetChild(0).GetComponent<Image>();
+    }
+
+    private void UpdateHealthBar(float maxHealth, float currentHealth, Faction faction)
+    {   
+        switch (faction)
+        {
+            case Faction.Enemy:
+                enemyHealthSprite.fillAmount = currentHealth / maxHealth;
+                break;
+            case Faction.Hero:
+                playerHealthSprite.fillAmount = currentHealth / maxHealth;
+                break;
+        }
     }
 
 
