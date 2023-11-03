297dc78b9e48480304b5a4448bdb5d17358cabae
Sergey Zaytsev <surgery_333@mail.ru>
2023-11-03 13:42:55 +0300

    Comment to class in SpawnUnitBlank added

--- Assets/_Scripts/Model/SpawnUnitBlank.cs
+++ Assets/_Scripts/Model/SpawnUnitBlank.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
+// ƒанный класс отвечает за логику перетаскивани€ спрайта юнита из меню выбора юнитов, что позвол€ет
+// реализовать "Drag and Drop" систему. Ћогика така€:
+
+// ¬ OnBeginDrag(при начале перетаскивани€) мы мен€ем родител€ у спрайта юнита в UnitSelectionMenu,
+// предварительно сохранив изначального родител€,и назначем нового родител€,а именно root(корневой объект, т.е. сам Canvas).
+// —делано дл€ того, чтобы остальные элементы UI не перекрывали спрайт, который тащим(т.е. чтобы он был в самом низу Canvas по иерархии).
+// “акже выставл€ем значение Raycast Target на false, чтобы сам спрайт не перекрывал собой дл€ курсора Tile, на который
+// хотим дропнуть спрайт дл€ спавна фигурки, иначе Event OnDrop в Tile не сработает.
+
+// ¬ OnEndDrag(как заканчиваем тащить) мы задаЄм родител€ до перетаскивани€ и вовзращаем значение Raycast Target на true.
 public class SpawnUnitBlank : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     public Image image;
@@ -15,6 +25,7 @@ public class SpawnUnitBlank : MonoBehaviour, IBeginDragHandler, IDragHandler, IE
         parentAfterDrag = transform.parent;
         transform.SetParent(transform.root);
         transform.SetAsLastSibling();
+
         image.raycastTarget = false;
     }
 
@@ -28,6 +39,7 @@ public class SpawnUnitBlank : MonoBehaviour, IBeginDragHandler, IDragHandler, IE
     {
         Debug.Log($"EndDrag {image.name}");
         transform.SetParent(parentAfterDrag);
+
         image.raycastTarget = true;
     }
 }
